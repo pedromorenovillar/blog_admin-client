2dbedc110292b4ea03e9085b3469e94bbdd01e51
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-04 16:29:49 +0200

    style: add navigation bar style

--- src/components/layout/NavigationBar.jsx
+++ src/components/layout/NavigationBar.jsx
@@ -2,6 +2,7 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import styles from "./NavigationBar.module.css";
+import { UserStar, StickyNote, Eye, LogOut } from "lucide-react";
 const CLIENT_URL = import.meta.env.VITE_CLIENT_URL;
 
 function NavigationBar() {
@@ -18,12 +19,24 @@ function NavigationBar() {
   }
   return (
     <div className={styles.navBar}>
-      <NavLink to="/">Dashboard</NavLink>
+      <NavLink to="/">
+        <UserStar />
+        Dashboard
+      </NavLink>
       {isAuthenticated ? (
         <>
-          <NavLink to="/posts/new-post">New Post</NavLink>
-          <a href={`${CLIENT_URL}/posts`}>View Blog</a>
-          <button onClick={handleLogout}>Logout</button>
+          <NavLink to="/posts/new-post">
+            <StickyNote />
+            New Post
+          </NavLink>
+          <a href={`${CLIENT_URL}/posts`}>
+            <Eye />
+            View Blog
+          </a>
+          <button onClick={handleLogout}>
+            <LogOut />
+            Logout
+          </button>
         </>
       ) : (
         <>
