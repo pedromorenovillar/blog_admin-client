5980cb055ff9bb558d4148e05c130809b4a8238c
Pedro José Moreno Villar <pjmorenovillar@gmail.com>
2026-08-10 17:44:55 +0200

    feat: add production server for frontend

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.static("dist"));
+
+app.get("/*splat", (req, res) => {
+  res.sendFile("index.html", { root: "dist" });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}.`);
+});
